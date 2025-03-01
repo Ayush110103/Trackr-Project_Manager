f22e99492e4370a9c78f7eedb83df2dbc8136b21
test <jain.68@iitj.ac.in>
2025-03-01 11:05:33 +0530

    Update header.jsx

--- components/header.jsx
+++ components/header.jsx
@@ -9,11 +9,12 @@ import { checkUser } from "@/lib/checkUser";
 import UserLoading from "./user-loading";
 import { BackgroundGradient } from "@/components/ui/background-gradient";
 import { HoverBorderGradient } from "@/components/ui/hover-border-gradient";
+
 async function Header() {
   await checkUser();
 
   return (
-    <header className="container mx-auto">
+    <header className="sticky top-0 z-50 container mx-auto">
       <nav className="py-6 px-4 flex justify-between items-center">
         <Link href="/">
           <h1 className="text-2xl font-bold">
@@ -28,22 +29,22 @@ async function Header() {
         </Link>
         <div className="flex items-center gap-4">
           <Link href="/project/create">
-          <button className="relative inline-flex h-12 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
-                <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
-                 <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl">
-                   Create Project
-             </span>
-               </button>
+            <button className="relative inline-flex h-12 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
+              <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
+              <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl">
+                Create Project
+              </span>
+            </button>
           </Link>
           <SignedOut>
             <SignInButton forceRedirectUrl="/onboarding">
-            <HoverBorderGradient
-                  containerClassName="rounded-full"
-                  as="button"
-                  className="relative inline-flex h-12 items-center justify-center bg-slate-950 px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50"
-                >
-                  Login
-                </HoverBorderGradient>
+              <HoverBorderGradient
+                containerClassName="rounded-full"
+                as="button"
+                className="relative inline-flex h-12 items-center justify-center bg-slate-950 px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50"
+              >
+                Login
+              </HoverBorderGradient>
             </SignInButton>
           </SignedOut>
           <SignedIn>
@@ -58,5 +59,3 @@ async function Header() {
 }
 
 export default Header;
-
-
